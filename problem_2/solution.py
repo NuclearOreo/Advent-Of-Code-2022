e31fc5034708cba42334a64364909c5e7bd7be76
Ussama Azam <nuclearoreo@gmail.com>
2022-12-06 14:54:44 -0500

    Solved the problem

--- problem_2/solution.py
+++ problem_2/solution.py
@@ -1,24 +1,20 @@
 file = open('./input.txt', 'r')
 
 mapping = { 'A': 'Rock', 'B': 'Paper', 'C': 'Scissors', 'X': 'Rock', 'Y': 'Paper', 'Z': 'Scissors' }
-beats = { 'Rock': 'Scissors', 'Paper': 'Rocks', 'Scissors': 'Paper' }
+beats = { 'Rock': 'Scissors', 'Paper': 'Rock', 'Scissors': 'Paper' }
 weights = { 'Rock': 1, 'Paper': 2, 'Scissors': 3 }
-win, lose, draw = 6, 0, 3
+win, draw = 6, 3
 score = 0
 
 for line in file:
-    player1, player2 = line.strip().split(' ')
-    player1, player2 = mapping[player1], mapping[player2]
+    opponent, player = line.strip().split(' ')
+    opponent, player = mapping[opponent], mapping[player]
 
-    score += weights[player2]
+    score += weights[player]
 
-
-    if beats[player1] == player2:
+    if opponent == player:
+        score += draw    
+    elif beats[player] == opponent:
         score += win
-    elif beats[player2] == player1:
-        score += lose
-    else:
-        score += draw
-
-print(f'Total Score: {score}')
     
+print(f'Total Score: {score}')    
