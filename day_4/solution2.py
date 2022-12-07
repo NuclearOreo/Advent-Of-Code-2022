4b85ab9282cfa1e0de4a441f8ce19c778dbd6b9d
Ussama Azam <nuclearoreo@gmail.com>
2022-12-07 12:01:21 -0500

    Solution 2 in day 4

--- day_4/solution2.py
+++ day_4/solution2.py
@@ -1,12 +1,12 @@
 file = open('./input.txt', 'r')
 
-totalFullOverlaps = 0
+partialOverlap = 0
 
 def doesFullyOverlap(a: list[int], b: list[int]) -> bool:
-    if  b[0] <= a[0] <= b[1] and b[0] <= a[1] <= b[1]:
+    if  b[0] <= a[0] <= b[1] or b[0] <= a[1] <= b[1]:
         return True
 
-    if a[0] <= b[0] <= a[1] and a[0] <= b[1] <= a[1]:
+    if a[0] <= b[0] <= a[1] or a[0] <= b[1] <= a[1]:
         return True
 
     return False
@@ -15,7 +15,6 @@ for line in file:
     r1, r2 = line.strip().split(',')
     r1, r2 = list(map(int, r1.split('-'))), list(map(int, r2.split('-')))
 
-    totalFullOverlaps += doesFullyOverlap(r1, r2)
+    partialOverlap += doesFullyOverlap(r1, r2)
 
-print(f'Total Full Overlaps: {totalFullOverlaps}')
-
+print(f'Total Partial Overlap: {partialOverlap}')
