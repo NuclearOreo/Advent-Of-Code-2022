337b5aa0edcb3335bd96cead73cb91a2a52c30d3
Ussama Azam <nuclearoreo@gmail.com>
2022-12-07 17:18:17 -0500

    Built graph

--- day_7/solution1.py
+++ day_7/solution1.py
@@ -2,19 +2,31 @@ from collections import defaultdict, deque
 
 file = open('./input.txt', 'r')
 
-tree = defaultdict(dict)
+class Directory:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.contents = []
+        self.dirs = {}
+
+root = Directory('/', None)
+current = root
 
 for line in file:
     command = line.strip().split(' ')
-
+    
     if command[0] == '$':
         if command[1] == 'cd':
-            if command[2] == '..':
-                current = parents[current]
-            elif command[2] == '/':
-                current = tree
+            f = command[2] 
+            if f == '..':
+                current = current.parent
+            elif f == '/':
+                current = root
+            else:
+                if f in current.dirs:
+                    current = current.dirs[f]
     else:
         if command[0] == 'dir':
-            parents[command[1]] = current
-            current[command[1]] = defaultdict(dict)
-
+            current.dirs[command[1]] = Directory(command[1], current)
+        else:
+            current.contents.append((int(command[0]), command[1]))
