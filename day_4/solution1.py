f7d81be8b6d1e00824207d4620d86bc952ca9a62
Ussama Azam <nuclearoreo@gmail.com>
2022-12-07 11:56:34 -0500

    Solution 1 for day 4

---
+++ day_4/solution1.py
@@ -0,0 +1,20 @@
+file = open('./input.txt', 'r')
+
+totalFullOverlaps = 0
+
+def doesFullyOverlap(a: list[int], b: list[int]) -> bool:
+    if  b[0] <= a[0] <= b[1] and b[0] <= a[1] <= b[1]:
+        return True
+
+    if a[0] <= b[0] <= a[1] and a[0] <= b[1] <= a[1]:
+        return True
+
+    return False
+
+for line in file:
+    r1, r2 = line.strip().split(',')
+    r1, r2 = list(map(int, r1.split('-'))), list(map(int, r2.split('-')))
+
+    totalFullOverlaps += doesFullyOverlap(r1, r2)
+
+print(f'Total Full Overlaps: {totalFullOverlaps}')
